1a644729b7a8bcc5d66bcf98a85c300ec3ba7217
Goreth Manuel <goreth.costa@mamboo.co.ao>
2025-04-08 17:27:25 +0100

    (feature-add): Adiciona o evento que pega os valores digitados :sparkles:

--- src/toDo/index.tsx
+++ src/toDo/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+import * as S from './style'
+
+
 const ToDo = () => {
+    const [task, setTask] = useState('');
+    console.log(task)
+
     return(
-        <>
-     <h1>Todo</h1>
-        </>
+        <S.Container> 
+        <form>
+            <input 
+            type="text" 
+            placeholder="Insira o nome da tarefa"
+            onChange={(event) => setTask(event.target.value) }
+            />
+            <button type="submit">Adicionar</button>
+        </form>
+        </S.Container>
+    
     )
 }
 export default ToDo;
---
+++ src/toDo/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+form {
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+
+    width: 100%;
+    max-width: 500px;
+}
+`
